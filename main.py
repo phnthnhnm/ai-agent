da3fb30ee6010a8f41c41d1c91b03d5466a794e4
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-08-18 13:06:34 +0700

    feat: Update content generation to accept user prompt from command line

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+if len(sys.argv) < 2:
+    print(f'Usage: {sys.argv[0]} "<prompt>"')
+    sys.exit(1)
+
+prompt = sys.argv[1]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=prompt,
 )
 
 print(response.text)
